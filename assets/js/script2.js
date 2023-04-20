d4856b20eb7c06bc878fa15c065c3630ce48ae0a
Katsuyuki <katsuyuki.russell@gmail.com>
2023-04-20 18:30:30 -0500

    fixed dayjs

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -4,17 +4,12 @@ var today = dayjs();
 var dayWeek = today.format('ddd');
 $('#today').text(dayWeek);
 
-// var tomorrow = today.add(1);
-// tomorrow.format('ddd')
-// $('#tomorrow').text(tomorrow);
-
 var tomorrow = today.add(1, 'days');
-var reformTomorrow = 
-$('#tomorrow').text(tomorrow);
+
+$('#tomorrow').text(tomorrow.format('ddd'));
 
 var twodays = today.add(2, 'days');
-$('#2days').text(twodays);
+$('#2days').text(twodays.format('ddd'));
 
-// 3. Parse the following date, 11/3/2020, and convert it into the following format: Sunday, February 14 2010, 3:25:50 pm.
-var reformatDate = dayjs('2020-11-03').format('dddd, MMMM D YYYY, h:mm:ss a');
-$('#3a').text(reformatDate);
+var threedays = today.add(3, 'days');
+$('#3days').text(threedays.format('ddd'));
